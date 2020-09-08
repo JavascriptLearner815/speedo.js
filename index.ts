c0dc3fa9571c4fcc195ef2b46a6464fa881a2f3a
Dean Summer <deanlovesmargie@gmail.com>
2020-09-07 22:47:49 -0500

    Add more properties and another class

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 module.exports = {
     Version: "1.0.0",
-    Color: "red",
+    Color: "Red",
     Exit: class Exit {
         code: number;
         constructor(code: number) {
@@ -8,6 +8,39 @@ module.exports = {
 
             console.warn(`Process exiting with exit code ${this.code}`);
             process.exit(this.code);
+        }
+    },
+    Username: "SpeedoThreeSixty",
+    Pets: [
+        "Daisy",
+        "Jack",
+        "Leo",
+        "Mia"
+    ],
+    Title: [
+        "Developer",
+        "Mathematician"
+    ],
+    Owner: "JavascriptLearner815",
+    ModuleLanguage: "TypeScript",
+    ProgramLanguage: "JavaScript",
+    Prompt: class Prompt {
+        text?: string;
+        value?: string;
+        prompt: string;
+        constructor(text?: string, value?: string) {
+            this.text = text;
+            this.value = value;
+            
+            if (this.text && this.value) {
+                this.prompt = prompt(this.text, this.value);
+            } else if (this.text) {
+                this.prompt = prompt(this.text);
+            } else if (this.value) {
+                this.prompt = prompt(null, this.value);
+            } else {
+                this.prompt = prompt(null);
+            }
         }
     }
 };
